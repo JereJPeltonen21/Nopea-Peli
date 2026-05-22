1e6b183fd1ba1cdb01b64c8e2482b9f0be33f0f3
JereJuhani02 <Jere.J.Peltonen@student.laurea.fi>
2026-05-22 16:12:51 +0300

    Pelin alku ja ensimmäinen peli luotu

---
+++ nopeapeli.py
@@ -0,0 +1,46 @@
+import random
+
+"""
+while True:
+    kysymys = input("Haluatko aloittaa pelin, Kyllä = k, Ei = e: ")
+    if kysymys == "e":
+        print("Nähdään seuraavan kerran")
+        break
+    elif kysymys == "k":
+        print("Aloitetaan peli")
+    else:
+        print("Vastaa, Kyllä = k tai Ei = e")
+"""
+
+def prosentti():
+    yritykset = 0
+    vastaus = random.randint(1,100)
+    while True:
+        print("#" * 20)
+        print(" ")
+        summa = int(input("Anna numero 1 - 100: "))
+        print(" ")
+        yritykset += 1
+
+        if summa > vastaus:
+            print("Liian suuri")
+            print(" ")
+        if summa < vastaus:
+            print("Liian pieni")
+            print(" ")
+        if summa >= 100:
+            print("Anna numero 1-100")
+            print(" ")
+        if summa == vastaus:
+            print(f"Oikein vastaus on {vastaus}")
+            print(" ")
+            break
+        if yritykset == 5:
+            print(f"Yritykset loppu, vastaus oli {vastaus}")
+            print(" ")
+            break
+            
+        print(f"Yrityksiä käytetty {yritykset}")
+        print(" ")
+
+prosentti()
